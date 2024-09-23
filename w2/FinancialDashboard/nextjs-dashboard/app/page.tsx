1b28ab902a7dd96abcce232461ce657e61abe00c
abrenzink <a.brenzink@gmail.com>
2024-09-23 18:29:50 -0300

    adding second image

--- w2/FinancialDashboard/nextjs-dashboard/app/page.tsx
+++ w2/FinancialDashboard/nextjs-dashboard/app/page.tsx
@@ -32,6 +32,8 @@ export default function Page() {
           {/* Add Hero Images Here */}
           <Image src="/hero-desktop.png" width={1000} height={760} className="hidden md:block" alt="Screenshots of the dashboard project showing desktop version"
           />
+          <Image src="/hero-mobile.png" width={560} height={620} className="block md:hidden" alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
